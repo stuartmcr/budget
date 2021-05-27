3ef3ad75f9444914f7661d20945c3e2a8c4d3a84
stuartmcr <simcrobbie@gmail.com>
2021-05-27 15:06:07 -0500

    read from csv

--- spending.py
+++ spending.py
@@ -1,3 +1,29 @@
 # pull in csv data with transactions
 
-print("Hello, World!")
+import csv
+
+print("---spending analysis---")
+
+with open('2018_data.csv', 'r') as csv_2018_Data:
+    csv_reader = csv.reader(csv_2018_Data)
+
+    next(csv_reader)
+
+    for line in csv_reader:
+        print(line[3])
+
+
+# 0: Date
+# 1: Description
+# 2: Category
+# 3: Amount
+# 4: Note
+# 5: Account
+# 6: Account #
+# 7: Institution
+# 8: Full Description
+# 9: Check Number
+# 10: Month
+# 11: Week
+# 12: Transaction ID
+# 13: Date Added
