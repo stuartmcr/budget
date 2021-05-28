5536ea0488aafbfabcdd17fc801192c2ddfd2e51
stuartmcr <simcrobbie@gmail.com>
2021-05-27 19:47:41 -0500

    updated readme and spending categories added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # budget
 Updating budget from excel to code
+
+Special thanks to TillerHQ for being great budgeting software and allowing me to gather all my spending data
---
+++ spending.py
@@ -0,0 +1,31 @@
+import csv
+
+""" spending categories:
+
+---NECESSARY---
+1.) Groceries
+2.) Rent
+3.) Utilities
+4.) Transportation
+5.) Medical
+6.) Education
+7.) Phone
+8.) Piper (dog)
+
+---DISCRETIONARY---
+1.) Restaurants
+2.) Drinks
+3.) Holidays
+4.) Fitness
+5.) Hobbies
+6.) Clothes
+7.) Home Improvements
+8.) Subscriptions
+9.) Gifts
+10.) Extra
+
+"""
+
+
+with open('2018.csv', 'r') as infile:
+    reader = csv.reader(infile, delimiter = ',')
