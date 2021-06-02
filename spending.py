a3cb36be700725f2db50c3eab48e98f7578ab8c0
stuartmcr <simcrobbie@gmail.com>
2021-06-01 20:48:02 -0400

    simple but functional expense logging db set up and running

--- spending.py
+++ spending.py
@@ -1,30 +1,73 @@
 import sqlite3
+from transaction import Transaction
 
-#create and connect to database
-conn = sqlite3.connect('test.db')
+# create and connect to database
+# connect to ':memory:' instead of .db for a fresh memory instance database (good for testing)
+conn = sqlite3.connect(':memory:')
 
-#create cursor to execute commands
+# create cursor to execute commands
 c = conn.cursor()
 
 #create table to hold info
-'''
+
 c.execute("""CREATE TABLE spending (
     month text,
     description text,
     category text,
     amount integer
     )""")
-'''
 
-#c.execute("INSERT INTO spending VALUES('January', 'Potbellys', 'Food', 7)")
-#conn.commit()
+# added context manager 'with conn:', not needed for 'get' function
+def insert_expense(exp):
+    with conn:
+        c.execute("INSERT INTO spending VALUES(:month, :description, :category, :amount)", {'month': exp.month, 'description': exp.description, 'category': exp.category, 'amount': exp.amount})
 
-c.execute("SELECT * FROM spending WHERE category='Food'")
+def get_expense_by_type(category_type):
+    c.execute("SELECT * FROM spending WHERE category=:category", {'category': category_type})
+    return c.fetchall()
 
-print(c.fetchall())
 
-#commits current transaction
-conn.commit()
+def update_cost(exp, amount):
+    with conn:
+        c.execute("""Update spending SET amount = :amount WHERE month = :month AND category = :category""", {'month': exp.month, 'category': exp.category, 'amount': amount})
 
-#close connection
+def remove_expense(exp):
+    with conn:
+        c.execute("""DELETE from spending WHERE month = :month AND category = :category""", {'month': exp.month, 'category':exp.category})
+
+
+# Transaction: Month, Description, Category, Amount
+exp_1 = Transaction('March', 'Pizza', 'Food', 12)
+exp_2 = Transaction('March', 'Fifa Game', 'Hobbies', 60)
+
+insert_expense(exp_1)
+insert_expense(exp_2)
+
+expenses = get_expense_by_type('Food')
+print(expenses)
+
+update_cost(exp_1, 9)
+remove_expense(exp_2)
+
+expenses = get_expense_by_type('Food')
+print(expenses)
+
 conn.close()
+
+# c.execute("INSERT INTO spending VALUES(?, ?, ?, ?)", (exp_1.month, exp_1.description, exp_1.category, exp_1.amount))
+# conn.commit()
+
+# c.execute("INSERT INTO spending VALUES(:month, :description, :category, :amount)", {'month': exp_2.month, 'description': exp_2.description, 'category': exp_2.category, 'amount': exp_2.amount})
+# conn.commit()
+
+# c.execute("SELECT * FROM spending WHERE category=?", ('Food',))
+# print(c.fetchall())
+
+# c.execute("SELECT * FROM spending WHERE category=:category", {'category': 'Hobbies'})
+# print(c.fetchall())
+
+# commits current transaction
+# conn.commit()
+
+# close connection
+# conn.close()
--- transaction.py
+++ transaction.py
@@ -1,13 +1,14 @@
 class Transaction:
-    def __init__(self, date, description, category, amount):
-        self.date = date
+
+    def __init__(self, month, description, category, amount):
+        self.month = month
         self.description = description
         self.category = category
         self.amount = amount
     
     @property
     def budgeting(self):
-        return '{}.{}'.format(self.date, self.category)
+        return '{}.{}'.format(self.month, self.category)
     
     def __repr__(self):
-        return "Transaction('{}', '{}', '{}', {})".format(self.date, self.description, self. category, self.amount)
+        return "Transaction('{}', '{}', '{}', {})".format(self.month, self.description, self. category, self.amount)
